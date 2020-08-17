de3e186e74316f691ba1b14253cacc00b8d639c8
unknown <mark.dacek@richmond.edu>
2020-08-17 13:57:19 -0400

    initial refactor

--- src/main/java/org/apache/commons/lang3/StringUtils.java
+++ src/main/java/org/apache/commons/lang3/StringUtils.java
@@ -2721,25 +2721,22 @@ public class StringUtils {
         if (isEmpty(cs) || ArrayUtils.isEmpty(searchChars)) {
             return INDEX_NOT_FOUND;
         }
+        
+        Set<Character> set = new HashSet<>();
+        for(char c: searchChars) {
+        	set.add(c);
+        }
+        
         final int csLen = cs.length();
-        final int csLast = csLen - 1;
-        final int searchLen = searchChars.length;
-        final int searchLast = searchLen - 1;
+        
         for (int i = 0; i < csLen; i++) {
             final char ch = cs.charAt(i);
-            for (int j = 0; j < searchLen; j++) {
-                if (searchChars[j] == ch) {
-                    if (i < csLast && j < searchLast && Character.isHighSurrogate(ch)) {
-                        // ch is a supplementary character
-                        if (searchChars[j + 1] == cs.charAt(i + 1)) {
-                            return i;
-                        }
-                    } else {
-                        return i;
-                    }
-                }
+            
+            if(set.contains(ch)) {
+            	return i;
             }
         }
+        
         return INDEX_NOT_FOUND;
     }
 
